96ad8bd14c8fe011aee47b83ddc3150d092050fd
RISHICHAARY <rs3715@srmist.edu.in>
2024-01-10 09:26:46 +0530

    Initial Commit

---
+++ Greenwealth/GreenwealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenwealthApp.swift
+//  Greenwealth
+//
+//  Created by user1 on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GreenwealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
